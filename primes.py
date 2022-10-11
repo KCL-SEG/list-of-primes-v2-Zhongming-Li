2b86acf186b5a7c74a30ce5e523120ccde7fa7cd
Zhongming Li <zhongming.li@kcl.ac.uk>
2022-10-11 13:05:13 +0100

    Generate prime numbers

--- primes.py
+++ primes.py
@@ -3,4 +3,13 @@
 
 def primes(number_of_primes):
     list = []
+    current_num = 2
+    while (len(list) < number_of_primes):
+        for i in range (2, current_num+1):
+            if (i == current_num):
+                list.append(current_num)
+            elif (current_num % i == 0):
+                break
+        current_num += 1
+
     return list
